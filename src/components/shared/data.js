7bc711c48b26dbb109b48c623fa04c2c1823c7a7
vUnAmp <phanhaingoc@gmail.com>
2020-11-17 23:52:37 +0100

    added react-select for filter mobile items

---
+++ src/components/shared/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  [
+    { value: 'iphone11', label: 'Iphone 11' },
+    { value: 'iphone12', label: 'Iphone 12' },
+  ],
+  [
+    { value: 'galaxy20', label: 'Galaxy 20' },
+    { value: 'galaxy21', label: 'Galaxy 21' },
+  ],
+  [
+    { value: 'xiaomi100', label: 'Xiaomi 100' },
+    { value: 'xiaomi898', label: 'Xiaomi 898' },
+  ],
+];
+export const branches = [
+  { value: 'apple', label: 'Apple' },
+  { value: 'samsung', label: 'Samsung' },
+  { value: 'xiaomi', label: 'Xiaomi' },
+];
+
+export const problems = [
+  { value: 'Ladebuchse defekt', label: 'Ladebuchse defekt' },
+  { value: 'Kopfhörerbuchse defekt', label: 'Kopfhörerbuchse defekt' },
+  { value: 'Akku defekt', label: 'Akku defekt' },
+  { value: 'Glasbruch', label: 'Glasbruch' },
+  { value: 'Wasserschaden Diagnose', label: 'Wasserschaden Diagnose' },
+];
--- src/pages/termin.js
+++ src/pages/termin.js
@@ -1,20 +1,107 @@
-import React, { useState } from "react"
-import Layout from "../components/layout"
+import React, { useState } from 'react';
+import Select from 'react-select';
 
-import "./termin.scss"
-const d = new Date()
-const month = d.getMonth()
-const day = +d.getDate()
-const year = d.getFullYear()
-const fullDate = [year, month, day].join("-")
+import Layout from '../components/layout';
+
+import './termin.scss';
+import { data, branches, problems } from '../components/shared/data';
+const d = new Date();
+const month = d.getMonth();
+const day = +d.getDate();
+const year = d.getFullYear();
+const fullDate = [year, month, day].join('-');
+
 const Termin = () => {
-  const [terminDate, setTerminDate] = useState(`${fullDate}T00:00`)
+  // console.log(data[0]);
+  // console.log(branches);
+  const [terminDate, setTerminDate] = useState(`${fullDate}T00:00`);
+  const [branch, setBranch] = useState(null);
+  const [model, setModel] = useState(null);
+  const [moldeles, setModeles] = useState(null);
+  const [toggle, setToggle] = useState(true);
+  // const [branchpl, setBranchpl] = useState('2. Modell auswählen…');
+  const [problem, setProblems] = useState(null);
+  const handleBranches = (e) => {
+    const x = branches.findIndex((item) => item.label === e.label);
+    console.log(x);
+    // setModel(data[x]);
+    setBranch(e);
+    setToggle(false);
+    setModeles(data[x]);
+    console.log(data[x]);
+    setModel('2. Modell auswählen…');
+  };
+  const handleModel = (e) => {
+    setModel(e);
+  };
+  const handleProblem = (e) => {
+    setProblems(e);
+  };
   return (
     <Layout>
       <form action="" className="termin">
         <h2>Termin Buchen</h2>
-        <input type="text" placeholder="Your Name" />
-        <input type="text" placeholder="Your Phone Number" />
+        <input type="text" placeholder="Ihr Name" />
+        <input type="text" placeholder="Handy Numer" />
+        <Select
+          className="select select-branch"
+          value={branch}
+          onChange={handleBranches}
+          options={branches}
+          placeholder="1. Hersteller auswählen…"
+        />
+        <Select
+          className="select select-model"
+          value={model}
+          onChange={handleModel}
+          options={moldeles}
+          isDisabled={toggle}
+          placeholder="2. Modell auswählen…"
+        />
+        <Select
+          className="select select-problem"
+          onChange={handleProblem}
+          value={problem}
+          options={problems}
+          isDisabled={toggle}
+          placeholder="3. Problem auswählen…"
+        />
+        {/* <select
+          name="typeofhandy"
+          id="deinhandy"
+          onChange={handleBranches}
+          defaultValue="branches"
+        >
+          <option value="branches" disabled>
+            1. Hersteller auswählen…
+          </option>
+          {branches.map((item) => {
+            // console.log(item);
+            return (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+        <select name="" id="deinhandy-model" defaultValue="model">
+          <option value="model" disabled>
+            2. Modell auswählen…
+          </option>
+          {model &&
+            model.map((item, i) => {
+              return (
+                <option value={item} key={i}>
+                  {item}
+                </option>
+              );
+            })}
+        </select>
+        <select name="" id="deinhandy-problem" defaultValue="problem">
+          <option value="problem" disabled>
+            3. Problem auswählen…
+          </option>
+        </select> */}
         <textarea
           name=""
           id=""
@@ -29,23 +116,24 @@ const Termin = () => {
           name="meeting-time"
           // value="2018-06-12T19:30"
           value={terminDate}
-          onChange={e => {
-            setTerminDate(e.target.value)
+          onChange={(e) => {
+            setTerminDate(e.target.value);
           }}
           min="2020-06-07T00:00"
           max="2021-10-14T00:00"
         ></input>
+
         <button
-          onClick={e => {
-            e.preventDefault()
-            alert("Just a demo....^^")
+          onClick={(e) => {
+            e.preventDefault();
+            alert('Just a demo....^^');
           }}
         >
           Termin confirm
         </button>
       </form>
     </Layout>
-  )
-}
+  );
+};
 
-export default Termin
+export default Termin;
