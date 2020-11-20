0ff9c924d639e37d0a1748d034ff63bce76a7184
ChiTran <Khanhchi.tran13051@gmail.com>
2020-11-20 18:11:43 +0100

    added kontakt infomation to aboutus

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,12 +1,15 @@
-import React from "react"
-import Layout from "../components/layout"
-
+import React from "react";
+import Layout from "../components/layout";
+import { Handy, OpenTime, Address } from "../components/shared/Kontakt";
 const aboutus = () => {
   return (
     <Layout>
       <h1>Is building ....</h1>
+      <Handy />
+      <OpenTime />
+      <Address />
     </Layout>
-  )
-}
+  );
+};
 
-export default aboutus
+export default aboutus;
